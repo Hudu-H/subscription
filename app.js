4f76cf75d0aa5be0cfc5989e2c3615866b51e2e2
Hudu <92208883+Hudu-H@users.noreply.github.com>
2025-07-11 20:19:52 +0200

    implement error middlewares (#5)

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
+import cookieParser from 'cookie-parser';
 
 // internal imports
 import { PORT } from './config/env.js';
@@ -6,13 +7,22 @@ import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabse from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
+
+// express middlewares
+app.use(express.json);
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser);
 
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+// error middleware
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
 	res.send('welcome to your first backend course');
