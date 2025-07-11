e032de90a4d7751930c4dd92dd08041c6c37cb1a
Hudu <ahmedhudu21@gmail.com>
2025-07-11 16:41:54 +0200

    implement DB connection

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ import { PORT } from './config/env.js';
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabse from './database/mongodb.js';
 
 const app = express();
 
+// routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
@@ -16,8 +18,12 @@ app.get('/', (req, res) => {
 	res.send('welcome to your first backend course');
 });
 
-app.listen(PORT, () => {
+// listen
+app.listen(PORT, async () => {
 	console.log(`server running on http://localhost:${PORT}`);
+
+	// call DB connection
+	await connectToDatabse();
 });
 
 export default app;
